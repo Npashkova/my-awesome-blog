16f4c668267efe48a78bde4d4f218b68f0e9b641
Npashkova <pashkova.link@gmail.com>
2021-07-01 17:28:20 +0300

    second_commit

--- events/models.py
+++ events/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    event_image = models.ImageField(upload_to='event_image/')
+    event_caption = models.CharField(max_length=300)
